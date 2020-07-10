1ac985f3ae70df6a83d6b214a52a644a7c6dc44a
Christo De Lange <github@dldinternet.com>
2020-07-10 19:34:27 -0400

    Commit code before deployment of 1.1.2rev3+1 to DEV

--- awssso/__init__.py
+++ awssso/__init__.py
@@ -1,7 +1,1 @@
-from pkg_resources import get_distribution, DistributionNotFound
-
-try:
-    __version__ = '1.1.2rev3'
-except DistributionNotFound:
-    # package is not installed
-    pass
+__version__ = '1.1.2rev3'
